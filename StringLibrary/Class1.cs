2ec10a8798b4256599116aae9eaeb4f25526a587
Star <rubiyasultana0@gmail.com>
2026-04-08 15:52:47 -0400

    Add project files.

---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            return char.IsUpper(input[0]);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Hello";
+            Console.WriteLine(test.StartsWithUpperCase()); // true
+        }
+    }
+}
